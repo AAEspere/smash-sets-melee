ae4200a23442221bb93039cc62902563c47426b5
Aaron Espere <aespere0@gmail.com>
2020-08-28 18:36:36 -0400

    Setting up environment

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+app.use('/bootstrap-4.3.1-dist', express.static('boostrap-4.3.1-dist'));
+
+app.listen(port, () => {
+    console.log('Smash app listening at http://localhost:${port}');
+})
+
+//home page
+app.get('/', function (req, res) {
+    res.render('index');
+})
