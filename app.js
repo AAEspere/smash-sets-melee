f9b3f2e84c8ff099f8170a86adbb0db01f90f7e9
Aaron Espere <aespere0@gmail.com>
2020-09-02 20:18:21 -0400

    switching computers, uploading everything I have so far

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const app = express();
 const port = 3000;
 
 app.set('view engine', 'ejs');
-app.use('/bootstrap-4.3.1-dist', express.static('boostrap-4.3.1-dist'));
-app.use("/public",express.static("public")); 
+//app.use('/bootstrap-4.3.1-dist', express.static('boostrap-4.3.1-dist'));
+app.use('/public',express.static('public')); 
 
 app.listen(port, () => {
     console.log('Smash app listening at http://localhost:${port}');
